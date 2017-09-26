31e8618d61e2212b0f79306b624ecd0b2a566b4f
Bahdan Kutas <Bahdan_Kutas@epam.com>
2017-09-26 20:04:11 +0300

    task3/feature: moon

---
+++ homeworks/hw03/bahdan_kutas/src/moon-interactions.js
@@ -0,0 +1,11 @@
+let moon = document.querySelector('.moon-wraper');
+let showMoon = document.querySelector('.button__moon-switch');
+let hideMoon = document.querySelector('.moon__button');
+
+let moonSwitch = function() {
+    moon.classList.toggle('toggled');
+    document.body.classList.toggle('scroll-hander');
+}    
+
+showMoon.addEventListener('click', moonSwitch, false);
+hideMoon.addEventListener('click', moonSwitch, false);

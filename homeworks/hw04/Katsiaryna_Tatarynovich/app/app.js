562e626d8221ade0c1763139afe087e8d8220b01
Katsiaryna Tatarynovich <Katsiaryna_Tatarynovich@epam.com>
2017-10-05 19:22:09 +0300

    task05\feature: added number filter

--- homeworks/hw04/Katsiaryna_Tatarynovich/app/app.js
+++ homeworks/hw04/Katsiaryna_Tatarynovich/app/app.js
@@ -1,4 +1,4 @@
 angular.module('app', [
-    'ngMessages'
-    // 'card'
+    'ngMessages',
+    'card'
 ]);
--- homeworks/hw04/Katsiaryna_Tatarynovich/app/directives/valueInputValidatorDirective.js
+++ homeworks/hw04/Katsiaryna_Tatarynovich/app/directives/valueInputValidatorDirective.js
@@ -1,22 +1,54 @@
 'use strict';
 
-const valueInputValidatorDirective = () => {
+const valueInputValidatorDirective = ($filter) => {
     return {
         restrict: 'A',
         require: 'ngModel',
+        scope: {
+            ngModel: '='
+        },
         link: (scope, element, attr, ngModel) => {
             const numberMinValue = attr.minValue;
-            console.log(attr);
+            const numberMaxValue = attr.maxValue;
+            const patternValue = new RegExp(attr.customPattern);
+            const updateView = (value) => {
+                ngModel.$viewValue = value;
+                ngModel.$render();
+            };
 
-            ngModel.$validators.isLessThanMinValue = (modelValue, viewValue) => {
-                const priceValue = viewValue;
-                console.log('modelValue ' + modelValue + '; ' + 'viewValue ' + viewValue);
-                // return (priceValue < numberMinValue) ? true : false;
+            ngModel.$validators.isFutureDate = (modelValue, viewValue) => {
+                let priceValue = Number(modelValue);
+                let filterPriceValue = $filter('number')(priceValue);
+                let status = true;
+
                 if (priceValue < numberMinValue) {
                     ngModel.$setValidity('minValueValidator', false);
                 } else {
                     ngModel.$setValidity('minValueValidator', true);
                 }
+
+                if (priceValue > numberMaxValue) {
+                    ngModel.$setValidity('maxValueValidator', false);
+                } else {
+                    ngModel.$setValidity('maxValueValidator', true);
+                }
+
+                if (priceValue === null) {
+                    ngModel.$setValidity('isValueValidator', false);
+                } else {
+                    ngModel.$setValidity('isValueValidator', true);
+                }
+
+                if (!patternValue.test(priceValue) && priceValue !== 'undefined') {
+                    ngModel.$setValidity('patternValueValidator', false);
+                } else {
+                    ngModel.$setValidity('patternValueValidator', true);
+                }
+
+                element.on('focus', () => updateView(modelValue));
+                element.on('blur', () => updateView(filterPriceValue));
+
+                return status;
             };
         }
     };
@@ -24,4 +56,4 @@ const valueInputValidatorDirective = () => {
 
 angular
     .module('app')
-    .directive('valueInputValidator', valueInputValidatorDirective);
+    .directive('valueInputValidator', ['$filter', valueInputValidatorDirective]);

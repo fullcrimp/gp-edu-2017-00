94eca5a8e2468e8d5b6746f60cad47ac77e87d5f
Bahdan Kutas <Bahdan_Kutas@epam.com>
2017-10-23 15:26:00 +0300

    task4/fix

--- homeworks/hw04/bahdan_kutas/Component based/app/tests/inputcheck.validator.spec.js
+++ homeworks/hw04/bahdan_kutas/Component based/app/tests/inputcheck.validator.spec.js
@@ -8,16 +8,9 @@ describe('inputcheckDirective', () => {
         suit.scope = $rootScope.$new();
         suit.element = '<input ng-model="value" input-check-validator >';
         suit.element = $compile(suit.element)(suit.scope);
-        debugger;
-        console.log('0', suit.scope);
         suit.scope.$digest();
     }));
 
     it('it should behave somehow...', () => {
-        suit.scope
-        // console.dir('2', suit.scope);
     });
-    // it('should behave...', () => {
-    //     expect(2+2).toBe(4);
-    // });
 });
